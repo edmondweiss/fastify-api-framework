10926166b8b6cbd32d94bc39de644d0f45c108c8
edmondweiss <edmond.weiss@askapplications.com>
2022-05-08 10:46:08 -0600

    Update controller example

--- src/examples/controller.example.ts
+++ src/examples/controller.example.ts
@@ -1,5 +1,16 @@
-import { FastifyPluginAsync } from "fastify";
+import { Controller } from "../controllers/controller.type";
+import { FastifyInstance } from "fastify";
+import { Container } from "inversify";
+import { SampleService, SampleServiceIdentifier } from "./service.example";
 
-export const ControllerExample: FastifyPluginAsync = async (server, opts) => {
-  server.get("/sample", {}, (request, reply) => {});
+export const ControllerExample: Controller = async (
+  server: FastifyInstance,
+  container: Container
+) => {
+  // You can grab objects via the container and use them in the controllers.
+  const sampleService = container.get<SampleService>(SampleServiceIdentifier);
+
+  server.get("/sample", {}, (request, reply) => {
+    sampleService.toString();
+  });
 };

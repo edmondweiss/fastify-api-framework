7cac8a99e965f6e0484a719377f0b346b535da77
Edmond Weiss <edmondweiss@gmail.com>
2023-06-04 09:44:19 -0400

    Close resources when termination signal is received

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,38 @@ import { app } from "./server/app.js";
 import { printAppConfig, printServerInfo } from "./utils/app.util.js";
 import type { AppConfig } from "./types/app-config.types.js";
 import { appConfigIdentifier } from "./config/identifiers.js";
+import { FastifyInstance } from "fastify";
+import { Container } from "inversify";
 
-app()
-  .then(([server, container]) => {
+async function close(
+  signal: string,
+  server: FastifyInstance,
+  container: Container
+) {
+  console.log(`\n${signal} signal received.`);
+  await container.unloadAsync();
+  console.log("Container unloaded.");
+  server.close(() => {
+    console.log("Server closed.");
+  });
+}
+async function main() {
+  let server: FastifyInstance;
+  let container: Container;
+
+  try {
+    [server, container] = await app();
     printAppConfig(container.get<AppConfig>(appConfigIdentifier));
     printServerInfo(server);
-  })
-  .catch((err) => {
+  } catch (err) {
     console.log("An application error occurred.");
     console.error(err);
     process.exit(1);
-  });
+  }
+
+  process.on("SIGINT", (signal) => close(signal, server, container));
+  process.on("SIGTERM", (signal) => close(signal, server, container));
+  process.on("SIGQUIT", (signal) => close(signal, server, container));
+}
+
+await main();
